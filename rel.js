ad58d11426531537b1ad050b884f36108ae32a6c
Moutie <108123837+Moutie12@users.noreply.github.com>
2022-08-11 03:27:39 +0200

    Add files via upload

---
+++ rel.js
@@ -0,0 +1,10 @@
+let next = document.getElementById("next");
+function clicker() {
+    next.style.backgroundColor ="#E3F510";
+}
+next.addEventListener("click", clicker,{once:true});
+let back = document.getElementById("back");
+function clicker1() {
+    back.style.backgroundColor ="#E3F510";
+}
+back.addEventListener("click", clicker1,{once:true});
---
+++ rel1.js
@@ -0,0 +1,31 @@
+let input1 = document.getElementById("input1");
+function input12() {
+    input1.style.boxShadow = " 2px 2px 52px 30px #0080FF" ;
+    input4.style.boxShadow ="none";
+    input2.style.boxShadow ="none";
+    input3.style.boxShadow ="none";
+}
+/////////////////////////////////////
+let input2 = document.getElementById("input2");
+function input123() {
+    input2.style.boxShadow = " 2px 2px 52px 30px #0080FF" ;
+    input3.style.boxShadow ="none";
+    input1.style.boxShadow ="none";
+    input4.style.boxShadow ="none";
+}
+///////////////////////////////////////
+let input3 = document.getElementById("input3");
+function input124() {
+    input3.style.boxShadow = " 2px 2px 52px 30px #0080FF" ;
+    input1.style.boxShadow ="none";
+    input2.style.boxShadow ="none";
+    input4.style.boxShadow ="none";
+}
+///////////////////////////////////////
+let input4 = document.getElementById("input4");
+function input12345() {
+    input4.style.boxShadow = " 2px 2px 52px 30px #0080FF" ;
+    input1.style.boxShadow ="none";
+    input2.style.boxShadow ="none";
+    input3.style.boxShadow ="none";
+}
